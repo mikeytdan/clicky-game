8f2abe52c25dc17761b41b5b3417b94b22a8c7fb
Michael Daniels <mikeytdan@gmail.com>
2018-09-11 21:26:02 -0600

    Removed Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Game from "./pages/Game";
 
 const App = () => (
-  <Router>
+  // <Router>
     <div>
-      <Route exact path="/" component={Game} />
-      <Route exact path="/game" component={Game} />
+     <Game/>
+      {/* <Route exact path="/game" component={Game} /> */}
     </div>
-  </Router>
+  // </Router>
 );
 
 export default App;
